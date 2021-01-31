bdd7fc5fec9055ba94c44c5782dac623388d47ff
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-01-31 19:55:37 +0600

    pin matcher updated

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # pin-matcher
+<h1>Pin Matcher</h1>
+
+<p>A simple pin matcher using HTML, CSS and JavaScript</p>
+
+<p><a href="https://pallabbarman.github.io/pin-matcher/">Live Site</a></p>
--- script.js
+++ script.js
@@ -14,3 +14,38 @@ function generatePin() {
     const pinInput = document.getElementById('pin');
     pinInput.value = getPin();
 }
+
+// handle calculator button event 
+const buttonContainer = document.getElementById("digit-container");
+buttonContainer.addEventListener('click', function (event) {
+    const digit = event.target.innerText;
+    if (isNaN(digit)) {
+        if (digit === 'C') {
+            const typedInput = document.getElementById('typed-pin');
+            typedInput.value = '';
+        }
+    }
+    else {
+        const typedInput = document.getElementById('typed-pin');
+        typedInput.value = typedInput.value + digit;
+    }
+})
+
+//  verify pin 
+function verifyPin() {
+    const pin = document.getElementById('pin').value;
+    const typedPin = document.getElementById('typed-pin').value;
+    if (pin === typedPin) {
+        displayMatchResult('block', 'none');
+    }
+    else {
+        displayMatchResult('none', 'block');
+    }
+}
+
+function displayMatchResult(correctStatus, incorrectStatus) {
+    const correct = document.getElementById('correct-pin');
+    correct.style.display = correctStatus;
+    const incorrect = document.getElementById('incorrect-pin');
+    incorrect.style.display = incorrectStatus;
+}
